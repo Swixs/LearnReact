3fc14167bca923617842a350d094f2796ef3427a
Nikita <Nekit4spice@gmail.com>
2023-10-25 15:07:27 +0200

    fix name components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import ProfileLogo from "./Profile-user";
-import MainClass from './Profile.module.css'
-import MainInput from "./Profile-input/Profile-input";
+import ProfileClass from './Profile.module.css';
+import ProfileInput from "./Profile-input/Profile-input";
 import ProfilePost from "./Profile-user-posts/Profile-user-posts";
 
-const Main = () => {
-
-
-    return <main className={MainClass.main}>
+const Profile = () => {
+    return <main className={ProfileClass.main}>
         <div>
-            <img className={MainClass.background} src='https://img.freepik.com/free-vector/flat-design-mountain-landscape_23-2149161403.jpg'></img>
+            <img className={ProfileClass.background} src='https://img.freepik.com/free-vector/flat-design-mountain-landscape_23-2149161403.jpg'></img>
         </div>
         <ProfileLogo />
-        <MainInput />
+        <ProfileInput />
         <div className="posts">
             <ProfilePost />
         </div>
     </main>
 }
 
-export default Main
+export default Profile
